783ac79b9f6a87f3842c51b37001b5d0422e8d00
Ronydt <ronysantos.d18@gmail.com>
2026-02-06 01:37:38 -0300

    fix: convert frontend from submodule to regular folder

---
+++ frontend/src/components/Form/Fields/index.js
@@ -0,0 +1,6 @@
+import Features from './Features';
+import Preferences from './Preferences';
+import RecommendationType from './RecommendationType';
+
+export { Features, Preferences, RecommendationType };
+
